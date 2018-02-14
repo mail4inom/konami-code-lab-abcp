6565ddde150345a4d0caabe23c28bfae236e59f3
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-14 05:11:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+var index = 0;
+document.addEventListener("keydown", function(e) {
+  var key = parseInt(e.detail || e.which);
+  if(key === code[index]){
+    index++;
+    if(index === code.length-1){
+      window.alert("Yo Got It Good Job!");
+      index = 0;
+    }
+  }else{
+    index = 0;
+  }
+}, false);
 }
