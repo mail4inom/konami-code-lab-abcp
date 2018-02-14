5e02774376989a1a92b78be3f4c3e7070c60f48b
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-14 02:32:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,26 +1,38 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(e) {
   // Write your JavaScript code inside the init() function
-
+var konami = parseInt(e.detail || e.which);
+for(var i=0; i < code.length; i++){
+  if(konami === code[i]){
+    i++;
+    if(konami === code.length){
+      alert("Congratulation!")
+      i = 0;
+    }
+  }else{
+    i = 0;
+  }
 }
-if (window.addEventListener) {
-    // create the keys and konami variables
-    var keys = [],
-        konami = "38,38,40,40,37,39,37,39,66,65";
+}
+const alphabet = [65, 66, 67];
 
-    // bind the keydown event to the Konami function
-    window.addEventListener("keydown", function(e){
-        // push the keycode to the 'keys' array
-        keys.push(e.keyCode);
+// Keep track of index outside of the event handler.
+let index = 0;
 
-        // and check to see if the user has entered the Konami code
-        if (keys.toString().indexOf(konami) >= 0) {
-            // do something such as:
-             alert('Konami');
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
 
-            // and finally clean up the keys array
-            keys = [];
-        };
-    }, true);
-};
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
