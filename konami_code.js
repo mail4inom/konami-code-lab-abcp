1a8eec6fa06007bfbf9363dc41c201ffa9d8d4e7
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-14 01:59:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,17 +8,17 @@ if (window.addEventListener) {
     // create the keys and konami variables
     var keys = [],
         konami = "38,38,40,40,37,39,37,39,66,65";
-    
+
     // bind the keydown event to the Konami function
     window.addEventListener("keydown", function(e){
         // push the keycode to the 'keys' array
         keys.push(e.keyCode);
-        
+
         // and check to see if the user has entered the Konami code
         if (keys.toString().indexOf(konami) >= 0) {
             // do something such as:
             // alert('Konami');
-            
+
             // and finally clean up the keys array
             keys = [];
         };
