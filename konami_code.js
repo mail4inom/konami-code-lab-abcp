7832a90f446d74383cdb595f80c74f18b12d3615
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-14 02:34:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,5 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-var konami = parseInt(e.detail || e.which);
-for(var i=0; i < code.length; i++){
-  if(konami === code[i]){
-    i++;
-    if(konami === code.length){
-      alert("Congratulation!")
-      i = 0;
-    }
-  }else{
-    i = 0;
-  }
-}
+  
 }
